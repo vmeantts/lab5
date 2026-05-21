a41e62cc3f08013792297f7e9383efd39e69d913
vmeantts <ivm261107@gmail.com>
2026-05-21 23:42:41 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,165 @@
+﻿#include "polynomial.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    // ===== ПЕРВЫЙ МНОГОЧЛЕН =====
+
+    int degree1;
+
+    cout << "Введите степень первого многочлена: ";
+    cin >> degree1;
+
+    vector<int> v1(degree1 + 1);
+
+    cout << "Введите коэффициенты первого многочлена:\n";
+
+    for (int i = 0; i <= degree1; i++)
+    {
+        cin >> v1[i];
+    }
+
+    Polynomial<int> p1(v1);
+
+    // ===== ВТОРОЙ МНОГОЧЛЕН =====
+
+    int degree2;
+
+    cout << "\nВведите степень второго многочлена: ";
+    cin >> degree2;
+
+    vector<int> v2(degree2 + 1);
+
+    cout << "Введите коэффициенты второго многочлена:\n";
+
+    for (int i = 0; i <= degree2; i++)
+    {
+        cin >> v2[i];
+    }
+
+    Polynomial<int> p2(v2);
+
+    // ===== ВЫВОД МНОГОЧЛЕНОВ =====
+
+    cout << "\nПервый многочлен:\n";
+    cout << p1 << "\n";
+
+    cout << "\nВторой многочлен:\n";
+    cout << p2 << "\n";
+
+    // ===== СЛОЖЕНИЕ =====
+
+    cout << "\nСложение p1 + p2:\n";
+    cout << p1 + p2 << "\n";
+
+    // ===== ВЫЧИТАНИЕ =====
+
+    cout << "\nВычитание p1 - p2:\n";
+    cout << p1 - p2 << "\n";
+
+    // ===== УМНОЖЕНИЕ =====
+
+    cout << "\nУмножение p1 * p2:\n";
+    cout << p1 * p2 << "\n";
+
+    // ===== СРАВНЕНИЕ =====
+
+    cout << "\nПроверка операторов == и !=:\n";
+
+    if (p1 == p2)
+    {
+        cout << "Многочлены равны\n";
+    }
+    else
+    {
+        cout << "Многочлены не равны\n";
+    }
+
+    if (p1 != p2)
+    {
+        cout << "Оператор != работает\n";
+    }
+
+    // ===== += =====
+
+    Polynomial<int> temp1 = p1;
+
+    temp1 += p2;
+
+    cout << "\nПосле temp1 += p2:\n";
+    cout << temp1 << "\n";
+
+    // ===== *= =====
+
+    Polynomial<int> temp2 = p1;
+
+    temp2 *= p2;
+
+    cout << "\nПосле temp2 *= p2:\n";
+    cout << temp2 << "\n";
+
+    // ===== operator[] =====
+
+    cout << "\nКоэффициент при x^0:\n";
+    cout << p1[0] << "\n";
+
+    cout << "\nКоэффициент при x^1:\n";
+    cout << p1[1] << "\n";
+
+    cout << "\nКоэффициент при x^2:\n";
+    cout << p1[2] << "\n";
+
+    // ===== Degree =====
+
+    cout << "\nСтепень первого многочлена:\n";
+    cout << p1.Degree() << "\n";
+
+    // ===== operator() =====
+
+    int x;
+
+    cout << "\nВведите x для вычисления значения первого многочлена: ";
+    cin >> x;
+
+    cout << "Значение многочлена:\n";
+    cout << p1(x) << "\n";
+
+    // ===== DOUBLE =====
+
+    cout << "\n===== МНОГОЧЛЕН DOUBLE =====\n";
+
+    int degreeDouble;
+
+    cout << "Введите степень многочлена double: ";
+    cin >> degreeDouble;
+
+    vector<double> vd(degreeDouble + 1);
+
+    cout << "Введите коэффициенты:\n";
+
+    for (int i = 0; i <= degreeDouble; i++)
+    {
+        cin >> vd[i];
+    }
+
+    Polynomial<double> pd(vd);
+
+    cout << "\nМногочлен double:\n";
+    cout << pd << "\n";
+
+    double xd;
+
+    cout << "\nВведите x для многочлена double: ";
+    cin >> xd;
+
+    cout << "Значение многочлена:\n";
+    cout << pd(xd) << "\n";
+
+    return 0;
+}
+
+
+
+
---
+++ polynomial.h
@@ -0,0 +1,189 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class Polynomial
+{
+private:
+
+    vector<T> coeffs;
+
+    void normalize()
+    {
+        while (coeffs.size() > 1 &&
+            coeffs.back() == T(0))
+        {
+            coeffs.pop_back();
+        }
+    }
+
+public:
+
+    // Конструктор из вектора
+    Polynomial(const vector<T>& v)
+    {
+        coeffs = v;
+        normalize();
+    }
+
+    // Конструктор по умолчанию
+    Polynomial(T value = T(0))
+    {
+        coeffs.push_back(value);
+        normalize();
+    }
+
+    // Степень многочлена
+    int Degree() const
+    {
+        return coeffs.size() - 1;
+    }
+
+    // Оператор []
+    T operator[](int power) const
+    {
+        if (power < 0 ||
+            power >= coeffs.size())
+        {
+            return T(0);
+        }
+
+        return coeffs[power];
+    }
+
+    // Вычисление значения
+    T operator()(T x) const
+    {
+        T result = T(0);
+
+        T power = T(1);
+
+        for (int i = 0; i < coeffs.size(); i++)
+        {
+            result =
+                result + coeffs[i] * power;
+
+            power = power * x;
+        }
+
+        return result;
+    }
+
+    // ==
+    bool operator==(const Polynomial& other) const
+    {
+        return coeffs == other.coeffs;
+    }
+
+    // !=
+    bool operator!=(const Polynomial& other) const
+    {
+        return !(*this == other);
+    }
+
+    // +
+    Polynomial operator+(const Polynomial& other) const
+    {
+        int maxSize =
+            max(coeffs.size(),
+                other.coeffs.size());
+
+        vector<T> result(maxSize, T(0));
+
+        for (int i = 0; i < maxSize; i++)
+        {
+            if (i < coeffs.size())
+            {
+                result[i] += coeffs[i];
+            }
+
+            if (i < other.coeffs.size())
+            {
+                result[i] += other.coeffs[i];
+            }
+        }
+
+        return Polynomial(result);
+    }
+
+    // -
+    Polynomial operator-(const Polynomial& other) const
+    {
+        int maxSize =
+            max(coeffs.size(),
+                other.coeffs.size());
+
+        vector<T> result(maxSize, T(0));
+
+        for (int i = 0; i < maxSize; i++)
+        {
+            if (i < coeffs.size())
+            {
+                result[i] += coeffs[i];
+            }
+
+            if (i < other.coeffs.size())
+            {
+                result[i] -= other.coeffs[i];
+            }
+        }
+
+        return Polynomial(result);
+    }
+
+    // *
+    Polynomial operator*(const Polynomial& other) const
+    {
+        vector<T> result(
+            coeffs.size() + other.coeffs.size(),
+            T(0));
+
+        for (int i = 0; i < coeffs.size(); i++)
+        {
+            for (int j = 0;
+                j < other.coeffs.size();
+                j++)
+            {
+                result[i + j] +=
+                    coeffs[i] *
+                    other.coeffs[j];
+            }
+        }
+
+        return Polynomial(result);
+    }
+
+    // +=
+    Polynomial& operator+=(const Polynomial& other)
+    {
+        *this = *this + other;
+
+        return *this;
+    }
+
+    // *=
+    Polynomial& operator*=(const Polynomial& other)
+    {
+        *this = *this * other;
+
+        return *this;
+    }
+
+    // Вывод
+    friend ostream& operator<<(
+        ostream& out,
+        const Polynomial& p)
+    {
+        for (int i = p.coeffs.size() - 1;
+            i >= 0;
+            i--)
+        {
+            out << p.coeffs[i] << " ";
+        }
+
+        return out;
+    }
+};
