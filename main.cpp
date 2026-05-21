72192ae412a67a3a11ea32357211153bf26017a7
vmeantts <ivm261107@gmail.com>
2026-05-21 23:32:18 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+﻿#include "matrix.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    // ===== INT =====
+
+    int rows, cols;
+
+    cout << "Введите количество строк: ";
+    cin >> rows;
+
+    cout << "Введите количество столбцов: ";
+    cin >> cols;
+
+    Matrix<int> A(rows, cols);
+    Matrix<int> B(rows, cols);
+
+    cout << "\nВведите элементы матрицы A:\n";
+    cin >> A;
+
+    cout << "\nВведите элементы матрицы B:\n";
+    cin >> B;
+
+    cout << "\nМатрица A:\n";
+    cout << A;
+
+    cout << "\nМатрица B:\n";
+    cout << B;
+
+    Matrix<int> C = A + B;
+
+    cout << "\nA + B:\n";
+    cout << C;
+
+    Matrix<int> D = A * B;
+
+    cout << "\nA * B:\n";
+    cout << D;
+
+    int i, j;
+
+    cout << "\nВведите индекс строки: ";
+    cin >> i;
+
+    cout << "Введите индекс столбца: ";
+    cin >> j;
+
+    cout << "Элемент: " << A.Get(i, j) << "\n";
+
+
+    // ===== STRING =====
+
+    cout << "\n===== Строковые матрицы =====\n";
+
+    Matrix<string> S1(2, 2);
+    Matrix<string> S2(2, 2);
+
+    cout << "Введите элементы первой строковой матрицы:\n";
+    cin >> S1;
+
+    cout << "Введите элементы второй строковой матрицы:\n";
+    cin >> S2;
+
+    cout << "\nПервая матрица:\n";
+    cout << S1;
+
+    cout << "\nВторая матрица:\n";
+    cout << S2;
+
+    Matrix<string> S3 = S1 + S2;
+
+    cout << "\nСложение строковых матриц:\n";
+    cout << S3;
+
+    return 0;
+}
+
+
+
---
+++ matrix.h
@@ -0,0 +1,145 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class Matrix
+{
+private:
+
+    int rows;
+    int cols;
+
+    T** data;
+
+public:
+
+    // Конструктор
+    Matrix(int r = 0, int c = 0)
+    {
+        rows = r;
+        cols = c;
+
+        data = new T * [rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            data[i] = new T[cols];
+        }
+    }
+
+    // Конструктор копирования
+    Matrix(const Matrix& other)
+    {
+        rows = other.rows;
+        cols = other.cols;
+
+        data = new T * [rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            data[i] = new T[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                data[i][j] = other.data[i][j];
+            }
+        }
+    }
+
+    // Деструктор
+    ~Matrix()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            delete[] data[i];
+        }
+
+        delete[] data;
+    }
+
+    // Set
+    void Set(int i, int j, T value)
+    {
+        data[i][j] = value;
+    }
+
+    // Get
+    T Get(int i, int j) const
+    {
+        return data[i][j];
+    }
+
+    // Сложение матриц
+    Matrix operator+(const Matrix& other)
+    {
+        Matrix result(rows, cols);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result.data[i][j] =
+                    data[i][j] + other.data[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    // Умножение матриц
+    Matrix operator*(const Matrix& other)
+    {
+        Matrix result(rows, other.cols);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < other.cols; j++)
+            {
+                result.data[i][j] = 0;
+
+                for (int k = 0; k < cols; k++)
+                {
+                    result.data[i][j] +=
+                        data[i][k] * other.data[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // Вывод
+    friend ostream& operator<<(ostream& out,
+        const Matrix& m)
+    {
+        for (int i = 0; i < m.rows; i++)
+        {
+            for (int j = 0; j < m.cols; j++)
+            {
+                out << m.data[i][j] << " ";
+            }
+
+            out << "\n";
+        }
+
+        return out;
+    }
+
+    // Ввод
+    friend istream& operator>>(istream& in,
+        Matrix& m)
+    {
+        for (int i = 0; i < m.rows; i++)
+        {
+            for (int j = 0; j < m.cols; j++)
+            {
+                in >> m.data[i][j];
+            }
+        }
+
+        return in;
+    }
+}; 
